7c14fe2bcabdf7aa659b296aaecd5eab04647dd8
Alienware <moycarlos09@gmail.com>
2024-07-13 17:05:45 -0400

    v2

---
+++ Migrations/20240713202618_Initial.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Especies",
+                columns: table => new
+                {
+                    EspecieID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Clasificacion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especies", x => x.EspecieID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Habitats",
+                columns: table => new
+                {
+                    HabitatID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Clima = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habitats", x => x.HabitatID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animales",
+                columns: table => new
+                {
+                    AnimalID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    EspecieID = table.Column<int>(type: "int", nullable: false),
+                    HabitatID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animales", x => x.AnimalID);
+                    table.ForeignKey(
+                        name: "FK_Animales_Especies_EspecieID",
+                        column: x => x.EspecieID,
+                        principalTable: "Especies",
+                        principalColumn: "EspecieID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Animales_Habitats_HabitatID",
+                        column: x => x.HabitatID,
+                        principalTable: "Habitats",
+                        principalColumn: "HabitatID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animales_EspecieID",
+                table: "Animales",
+                column: "EspecieID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animales_HabitatID",
+                table: "Animales",
+                column: "HabitatID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animales");
+
+            migrationBuilder.DropTable(
+                name: "Especies");
+
+            migrationBuilder.DropTable(
+                name: "Habitats");
+        }
+    }
+}
